06d4ddc32c70145655ff6e05aedc767858656e6a
Ronak Sanpui <ronaksanpui@gmail.com>
2022-02-09 11:42:26 +0530

    implemented delete button

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import { useChromeStorageSync } from 'use-chrome-storage';
 
 function App() {
 
-  const [list] = useChromeStorageSync('trackingList', []);
+  const [list, setList] = useChromeStorageSync('trackingList', []);
+
+  const deleteFromList = (animeName: string) => {
+    const newList = list.filter((anime: { name: string, date: Date }) => anime.name !== animeName)
+    setList(newList);
+  }
+
   return (<div className='root'>
     {list.map((anime: { name: string, date: Date }) =>
-      <Item key={anime.name} animeName={anime.name} />
+      <Item
+        key={anime.name}
+        animeName={anime.name}
+        deleteFromList={deleteFromList} />
     )}
   </div>)
 
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,17 +3,19 @@ import "../Item.css"
 
 type Props = {
     animeName: string,
+    deleteFromList: (animeName: string) => void
 }
 
 export default function Item(props: Props) {
-    const [{ episode, time }] = useChromeStorageSync(props.animeName, -1);
+    const [{ episode, time }, setValue] = useChromeStorageSync(props.animeName, -1);
 
     const goToAnime = (event: React.MouseEvent<HTMLElement>) => {
         window.open(`https://animepahe.com/anime?resAnime=${props.animeName}&resEp=${episode}&resTime=${time}`, '_blank')
     }
 
     const delAnime = () => {
-
+        props.deleteFromList(props.animeName)
+        chrome.storage.sync.remove(props.animeName)
     }
 
     return (
