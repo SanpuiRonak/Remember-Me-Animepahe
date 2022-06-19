75a63676f5b6392a87aa7652f768b21736fca5d0
Ronak Sanpui <ronaksanpui@gmail.com>
2022-06-19 13:46:28 +0530

    fixed: extension will now search next page

--- resume.js
+++ resume.js
@@ -7,7 +7,7 @@ const params = Object.fromEntries(urlSearchParams.entries());
 
 if (params.resAnime && params.resEp && params.resTime) {
     const href = document.querySelector(`a[title="${params.resAnime}"]`).href;
-    window.location.replace(href + `?resEp=${params.resEp}&resTime=${params.resTime}`);
+    window.location.replace(href + `?resEp=${params.resEp}&resTime=${params.resTime}&page=1`);
 }
 else if (params.resEp && params.resTime) {
 
@@ -18,25 +18,29 @@ else if (params.resEp && params.resTime) {
         if (playTags.length !== 0) {
             //list loaded
             clearInterval(pollTimer);
+            let episodeFound = false;
             playTags.forEach(aTag => {
                 //Look for episode span
                 if (aTag.innerText.includes(`- ${params.resEp} `)) {
+                    console.log("Episode Found");
                     window.location.replace(aTag.href + `?resTime=${params.resTime}`);
+                    episodeFound = true;
                 }
-
             })
-
-
+            //If it does not exsists go to next page
+            if (!episodeFound) {
+                console.log("going to next page...");
+                const nextPageUrl = window.location.href.replace(/page=\d/g, `page=${parseInt(params.page) + 1}`)
+                window.location.replace(nextPageUrl);
+            }
 
         }
     }, 1000);
 
 }
 else if (params.resTime) {
-    document.querySelector('div.click-to-load').click();
     const poller = setInterval(() => {
-        document.querySelector('iframe').contentWindow.postMessage({ payload:params.resTime , poller}, '*');
+        document.querySelector('iframe').contentWindow.postMessage({ payload: params.resTime, poller }, '*');
     }, 1000)
-
 }
 
